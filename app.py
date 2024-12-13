04d5b1e6fc2c821075bee0c516446566142a7c81
JAGATH KRISHNAN A P <112542747+JAGATHKRISHNANAP@users.noreply.github.com>
2024-12-13 16:39:25 +0530

    aws_port_change

--- app.py
+++ app.py
@@ -1931,7 +1931,7 @@ def upload_file_json():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=80)
     # app.run(debug=True)
 
 
